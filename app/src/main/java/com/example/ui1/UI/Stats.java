2256d27abc0187493167d4a1b78adf0e908d1399
Sanjula Thilakarathne <sanjulaprsan@gmail.com>
2022-05-16 11:42:42 +0530

    Minor changes

--- app/src/main/java/com/example/ui1/UI/Stats.java
+++ app/src/main/java/com/example/ui1/UI/Stats.java
@@ -28,7 +28,7 @@ public class Stats extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_stats);
 
         list=new ArrayList<>();
 
