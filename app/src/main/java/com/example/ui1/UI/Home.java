b84ce3fd1cdf28e5d89501637588a25d698c4f42
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-05-17 19:00:02 +0530

    Edit all interface

--- app/src/main/java/com/example/ui1/UI/Home.java
+++ app/src/main/java/com/example/ui1/UI/Home.java
@@ -2,9 +2,12 @@ package com.example.ui1.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -13,6 +16,12 @@ import com.example.ui1.R;
 public class Home extends AppCompatActivity {
 
     VideoView videoView;
+
+    private Button btnStats;
+    private  Button btnSelfAss;
+    private Button btnProf;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +32,7 @@ public class Home extends AppCompatActivity {
         this.videoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.blue_scan2));
         MediaController videoControl = new MediaController(this);
         videoControl.setAnchorView(this.videoView);
-        this.videoView.setMediaController(videoControl);
+        //this.videoView.setMediaController(videoControl);
         this.videoView.start();
 
         videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
@@ -32,6 +41,41 @@ public class Home extends AppCompatActivity {
                 mp.setLooping(true);
             }
         });
+
+        btnStats = (Button) findViewById(R.id.btnStats);
+        btnStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSituationReport();
+            }
+        });
+        btnSelfAss = (Button) findViewById(R.id.btnSelfAss);
+        btnSelfAss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSelfAss();
+            }
+        });
+        btnProf = (Button) findViewById(R.id.btnProf);
+        btnProf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProf();
+            }
+        });
+    }
+
+    public void openSituationReport(){
+        Intent intent = new Intent(this, SituationReport.class);
+        startActivity(intent);
+    }
+    public void openSelfAss(){
+        Intent intent = new Intent(this, SelfAssessment.class);
+        startActivity(intent);
+    }
+    public void openProf(){
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 
     @Override
--- app/src/main/java/com/example/ui1/UI/SelfAssessment.java
+++ app/src/main/java/com/example/ui1/UI/SelfAssessment.java
@@ -2,15 +2,32 @@ package com.example.ui1.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ui1.R;
 
 public class SelfAssessment extends AppCompatActivity {
 
+    private Button  btnHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_self_assessment);
+        btnHome = (Button) findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHome();
+            }
+        });
+    }
+
+    public void openHome(){
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
