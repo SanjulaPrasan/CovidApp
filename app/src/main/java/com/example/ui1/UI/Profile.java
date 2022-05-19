f5e5a5a1afd779000f57e2bee0a49ed5b37a678c
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-05-19 09:41:31 +0530

    Add data base

--- app/src/main/java/com/example/ui1/UI/Profile.java
+++ app/src/main/java/com/example/ui1/UI/Profile.java
@@ -1,17 +1,33 @@
 package com.example.ui1.UI;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ui1.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Profile extends AppCompatActivity {
 
     private Button btnHome;
+
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userId;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +40,42 @@ public class Profile extends AppCompatActivity {
                 openHome();
             }
         });
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userId = user.getUid();
+
+        //final TextView greetingTextView = (TextView) findViewById(R.id.greeting);
+        final TextView fullNameTextView = (TextView) findViewById(R.id.tvFullName);
+        final TextView emailTextView = (TextView) findViewById(R.id.tvEmail);
+        final TextView addressTextView = (TextView) findViewById(R.id.tvAdd);
+        final  TextView phoneTextView =(TextView) findViewById(R.id.tvPhone);
+
+        reference.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+                if (userProfile != null) {
+
+                    String fullname = userProfile.fullName;
+                    String email = userProfile.email;
+                    String phone = userProfile.phone;
+                    String address = userProfile.address;
+
+                    //greetingTextView.setText("Welcome, "+ fullname+ "!");
+                    fullNameTextView.setText(fullname);
+                    emailTextView.setText(email);
+                    addressTextView.setText(address);
+                    phoneTextView.setText(phone);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(Profile.this,"Something wrong happened!",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
     public void openHome(){
