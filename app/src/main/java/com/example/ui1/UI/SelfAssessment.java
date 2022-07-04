3143fa05c8be341ebd37b058b70f24c4c78fae0a
Sanjula Thilakarathne <sanjulaprsan@gmail.com>
2022-07-04 12:58:40 +0530

    collect data from SelfAssessment switch button fever

--- app/src/main/java/com/example/ui1/UI/SelfAssessment.java
+++ app/src/main/java/com/example/ui1/UI/SelfAssessment.java
@@ -1,9 +1,11 @@
 package com.example.ui1.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,12 +14,27 @@ import com.example.ui1.R;
 public class SelfAssessment extends AppCompatActivity {
 
     private Button  btnSubmit;
+    private SwitchCompat switchFever;
+    private int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_self_assessment);
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        switchFever = findViewById(R.id.switchBtnFever);
+        switchFever.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(switchFever.isChecked()){
+                    i = 1;
+                    Log.d("", "1 printed");
+                }else{
+                    i=0;
+                    Log.d("", "0 printed");
+                }
+            }
+        });
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
