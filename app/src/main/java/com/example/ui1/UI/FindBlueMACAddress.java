d5ac9110e903a61612a38b1223f2d2fc4a90d9ae
Sanjula Thilakarathne <sanjulaprsan@gmail.com>
2022-07-19 17:25:12 +0530

    Add Find MAC Address class

---
+++ app/src/main/java/com/example/ui1/UI/FindBlueMACAddress.java
@@ -0,0 +1,15 @@
+package com.example.ui1.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ui1.R;
+
+public class FindBlueMACAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_blue_macaddress);
+    }
+}
