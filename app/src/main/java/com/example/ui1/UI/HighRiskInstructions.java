61b0490a183f3d0c33e0b38cfd61c217c584216c
Sanjula Thilakarathne <sanjulaprsan@gmail.com>
2022-07-24 22:56:28 +0530

    High Risk Instruction java and xml added

---
+++ app/src/main/java/com/example/ui1/UI/HighRiskInstructions.java
@@ -0,0 +1,18 @@
+package com.example.ui1.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ui1.R;
+
+public class HighRiskInstructions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_risk_instructions);
+    }
+
+
+}
