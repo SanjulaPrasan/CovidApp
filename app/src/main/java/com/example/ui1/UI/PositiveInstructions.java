8899e889ead9ee036c2f86ab19be70f90748d0ad
Sanjula Thilakarathne <sanjulaprsan@gmail.com>
2022-07-24 21:53:22 +0530

    Positive Instruction java and xml added

---
+++ app/src/main/java/com/example/ui1/UI/PositiveInstructions.java
@@ -0,0 +1,21 @@
+package com.example.ui1.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ui1.R;
+
+public class PositiveInstructions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_positive_instructions);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
