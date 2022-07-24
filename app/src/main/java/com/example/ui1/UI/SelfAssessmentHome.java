a7f1b8a6aaf6b10e1021596d12c1b877944b81ab
Sanjula Thilakarathne <sanjulaprsan@gmail.com>
2022-07-24 11:44:25 +0530

    self assessment Home health status generation done

--- app/src/main/java/com/example/ui1/UI/SelfAssessmentHome.java
+++ app/src/main/java/com/example/ui1/UI/SelfAssessmentHome.java
@@ -7,7 +7,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.ui1.R;
@@ -15,123 +18,191 @@ import com.example.ui1.R;
 public class SelfAssessmentHome extends AppCompatActivity {
 
     private Button btnSubmit;
-    private Button  btnHome;
-    private SwitchCompat switchFever, switchCough, switchFatigue, switchBreathing, switchCovid;
-    private int i = 0;
+//    private Button  btnHome;
+    private Spinner spinnerFever, spinnerCough, spinnerDiarrhea, spinnerBodyPain, spinnerHeadache, spinnerLossOfSmell, spinnerRA, spinnerPCR;
+    private ArrayAdapter<CharSequence> feverAdapter, coughAdapter, diarrheaAdapter, bodyPainAdapter, headacheAdapter, lossOfSmellAdapter, raAdapter, pcrAdapter;
+    private SwitchCompat switchBreathing, switchConscious;
+    private String fever, cough, diarrhea, bodyPain, headache, lossOfSmell, rat, pcr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_self_assessment_home);
-        btnHome = (Button) findViewById(R.id.btnHome);
+//        btnHome = (Button) findViewById(R.id.btnHome);
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
-//        switchFever = findViewById(R.id.switchBtnFever);
-//        switchFever.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (switchCovid.isChecked()) {
-//                    switchFever.setChecked(true);
-//                } else {
-//                    if (switchFever.isChecked()) {
-//                        i++;
-//                    } else {
-//                        i--;
-//                    }
-//                }
-//            }
-//        });
-//        switchCough = findViewById(R.id.switchBtnCough);
-//        switchCough.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (switchCovid.isChecked()) {
-//                    switchCough.setChecked(true);
-//                } else {
-//                    if (switchCough.isChecked()) {
-//                        i++;
-//                    } else {
-//                        i--;
-//                    }
-//                }
-//            }
-//        });
-//        switchFatigue = findViewById(R.id.switchBtnFatigue);
-//        switchFatigue.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (switchCovid.isChecked()) {
-//                    switchFatigue.setChecked(true);
-//                } else {
-//                    if (switchFatigue.isChecked()) {
-//                        i++;
-//                    } else {
-//                        i--;
-//                    }
-//                }
-//            }
-//        });
-//        switchBreathing = findViewById(R.id.switchBtnBreathing);
-//        switchBreathing.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (switchCovid.isChecked()) {
-//                    switchBreathing.setChecked(true);
-//                } else {
-//                    if (switchBreathing.isChecked()) {
-//                        i++;
-//                    } else {
-//                        i--;
-//                    }
-//                }
-//            }
-//        });
-//        switchCovid = findViewById(R.id.switchBtnCovid);
-//        switchCovid.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (switchCovid.isChecked()) {
-//                    switchFever.setChecked(true);
-//                    switchCough.setChecked(true);
-//                    switchFatigue.setChecked(true);
-//                    switchBreathing.setChecked(true);
-//                    i = 5;
-//                } else {
-//                    switchFever.setChecked(false);
-//                    switchCough.setChecked(false);
-//                    switchFatigue.setChecked(false);
-//                    switchBreathing.setChecked(false);
-//                    i = 0;
-//                }
-//            }
-//        });
+        spinnerFever = (Spinner) findViewById(R.id.spinFever);
+        feverAdapter = ArrayAdapter.createFromResource(this, R.array.fever, android.R.layout.simple_spinner_item);
+        feverAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerFever.setAdapter(feverAdapter);
+        spinnerFever.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                fever = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinnerCough = (Spinner) findViewById(R.id.spinCough);
+        coughAdapter = ArrayAdapter.createFromResource(this, R.array.fever, android.R.layout.simple_spinner_item);
+        coughAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCough.setAdapter(coughAdapter);
+        spinnerCough.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                cough = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinnerDiarrhea = (Spinner) findViewById(R.id.spinDiarrhea);
+        diarrheaAdapter = ArrayAdapter.createFromResource(this, R.array.fever, android.R.layout.simple_spinner_item);
+        diarrheaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDiarrhea.setAdapter(diarrheaAdapter);
+        spinnerDiarrhea.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                diarrhea = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinnerBodyPain = (Spinner) findViewById(R.id.spinBodyPain);
+        bodyPainAdapter = ArrayAdapter.createFromResource(this, R.array.fever, android.R.layout.simple_spinner_item);
+        bodyPainAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerBodyPain.setAdapter(bodyPainAdapter);
+        spinnerBodyPain.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                bodyPain = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinnerHeadache = (Spinner) findViewById(R.id.spinHeadache);
+        headacheAdapter = ArrayAdapter.createFromResource(this, R.array.fever, android.R.layout.simple_spinner_item);
+        headacheAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerHeadache.setAdapter(headacheAdapter);
+        spinnerHeadache.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                headache = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinnerLossOfSmell = (Spinner) findViewById(R.id.spinLossSmell);
+        lossOfSmellAdapter = ArrayAdapter.createFromResource(this, R.array.fever, android.R.layout.simple_spinner_item);
+        lossOfSmellAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLossOfSmell.setAdapter(lossOfSmellAdapter);
+        spinnerLossOfSmell.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                lossOfSmell = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinnerRA = (Spinner) findViewById(R.id.spinRAT);
+        raAdapter = ArrayAdapter.createFromResource(this, R.array.test, android.R.layout.simple_spinner_item);
+        raAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerRA.setAdapter(raAdapter);
+        spinnerRA.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                rat = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spinnerPCR = (Spinner) findViewById(R.id.spinPCR);
+        pcrAdapter = ArrayAdapter.createFromResource(this, R.array.test, android.R.layout.simple_spinner_item);
+        pcrAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPCR.setAdapter(pcrAdapter);
+        spinnerPCR.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                pcr = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        switchBreathing = findViewById(R.id.switchBtnBreathing);
+        switchConscious = findViewById(R.id.switchConscious);
+
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                generateNewHealthStatus(i);
+                generateNewHealthStatus();
                 saveDataHome();
                 openHome();
             }
         });
 
-        btnHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openHome();
-            }
-        });
+//        btnHome.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openHome();
+//            }
+//        });
     }
 
-    public static void generateNewHealthStatus(int i){
-        if(i == 5){
+    public void generateNewHealthStatus(){
+        if(pcr.equals("Positive")){
             SelfAssessment.healthStatus = "POSITIVE";
-        }else if(i == 0){
+        } else if(pcr.equals("Negative")){
             SelfAssessment.healthStatus = "NEGATIVE";
-        }else if(i > 0 && i <3){
-            SelfAssessment.healthStatus = "LOW RISK";
-        }else{
-            SelfAssessment.healthStatus = "HIGH RISK";
+        } else {
+            if(rat.equals("Positive")){
+                SelfAssessment.healthStatus = "HIGH RISK";
+            } else if(rat.equals("Negative")){
+                SelfAssessment.healthStatus = "NEGATIVE";
+            } else{
+                if(switchBreathing.isChecked() || switchConscious.isChecked()){
+                    SelfAssessment.healthStatus = "HIGH RISK";
+                }else {
+                    if (fever.equals("No") && cough.equals("No") && diarrhea.equals("No") && bodyPain.equals("No") && headache.equals("No") && lossOfSmell.equals("No")) {
+                        SelfAssessment.healthStatus = "NEGATIVE";
+                    } else if (fever.equals("5-10 Days") || cough.equals("5-10 Days") || diarrhea.equals("5-10 Days") || bodyPain.equals("5-10 Days") || headache.equals("5-10 Days") || lossOfSmell.equals("5-10 Days")) {
+                        SelfAssessment.healthStatus = "HIGH RISK";
+                    } else if (fever.equals("1-5 Days") || cough.equals("1-5 Days") || diarrhea.equals("1-5 Days") || bodyPain.equals("1-5 Days") || headache.equals("1-5 Days") || lossOfSmell.equals("1-5 Days")) {
+                        SelfAssessment.healthStatus = "LOW RISK";
+                    }
+                }
+            }
         }
     }
 
