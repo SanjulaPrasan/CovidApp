ad78506c6bcd663a5e39b30c48a067be91af8b68
Sanjula Thilakarathne <sanjulaprsan@gmail.com>
2022-07-24 23:32:13 +0530

    Moderate Risk Instruction java and xml added done

---
+++ app/src/main/java/com/example/ui1/UI/ModerateRiskInstructions.java
@@ -0,0 +1,21 @@
+package com.example.ui1.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ui1.R;
+
+public class ModerateRiskInstructions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moderate_risk_instructions);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
