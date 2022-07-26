537484a7765fc5c6e13e0ad4070801f7cb30618a
Sanjula Thilakarathne <sanjulaprsan@gmail.com>
2022-07-26 21:42:39 +0530

    Added location permission into the beginning bluetooth class

--- app/src/main/java/com/example/ui1/UI/Bluetooth.java
+++ app/src/main/java/com/example/ui1/UI/Bluetooth.java
@@ -1,20 +1,26 @@
 package com.example.ui1.UI;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.ui1.R;
 
 public class Bluetooth extends AppCompatActivity {
-    public static final int REQUEST_ACCESS_COARSE_LOCATION = 1;
+    public static final int REQUEST_ACCESS_FINE_LOCATION = 1;
     public static final int REQUEST_ENABLE_BLUETOOTH = 11;
 
     private BluetoothAdapter bluetoothAdapter;
@@ -29,9 +35,34 @@ public class Bluetooth extends AppCompatActivity {
         btnBluetooth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkBluetoothState();
+                if (ContextCompat.checkSelfPermission(Bluetooth.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    checkBluetoothState();
+                } else {
+                    requestStoragePermission();
+                }
             }
         });
+    }
+
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Location Permission needed")
+                    .setMessage("This permission is needed to run this app")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(Bluetooth.this,
+                                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ACCESS_FINE_LOCATION);
+                        }
+                    })
+                    .create().show();
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ACCESS_FINE_LOCATION);
+        }
     }
 
     @SuppressLint("MissingPermission")
@@ -59,7 +90,17 @@ public class Bluetooth extends AppCompatActivity {
             startActivity(intent);
         }
         else {
-            Toast.makeText(this, "Need to turn on Bluetooth", Toast.LENGTH_LONG).show();
+            new AlertDialog.Builder(this)
+                    .setTitle("Bluetooth Permission needed")
+                    .setMessage("This permission is needed to run this app")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+                        }
+                    })
+                    .create().show();
         }
     }
 }
